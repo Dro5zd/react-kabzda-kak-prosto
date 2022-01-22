e4ec7883bf7e8c08879fdba0cc985b134d9bdbf5
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-01-22 13:55:30 +0600

    New

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,52 @@ import './App.css';
 function App() {
     return (
         <div>
-            This is APP component
+            <Rating/>
+            <Title/>
+            <Rating/>
+            <Accordion/>
         </div>
     )
-        ;
+}
+
+function Title() {
+    return (
+        <>This is APP component</>
+    )
+}
+
+
+function Rating() {
+    return (
+        <>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </>
+
+    )
+}
+
+function Star() {
+    return (
+        <div>star</div>
+    )
+}
+
+
+function Accordion() {
+    return (
+        <div>
+            <h3>Меню</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
 }
 
 export default App;
