50d23e64933f88e054c29976786ea72995395d7f
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-02-09 17:53:34 +0600

    homework06 trying reorg

---
+++ src/components/OnOff/OnOff.tsx
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+
+
+type OnOffPropsType = {
+    // on: boolean
+}
+
+
+
+
+export const OnOff = (props: OnOffPropsType) => {
+
+    let [on, setOn] = useState(false)
+
+const clickOnButton = ()=> {
+        setOn(true ? onStyle  : offStyle)
+}
+
+    const onStyle = {
+        height: '60px',
+        width: '150px',
+        border: '2px solid black ',
+        display: 'inline-block',
+        marginLeft: '50px',
+        backgroundColor: on ? 'green' : 'white'
+    }
+    const offStyle = {
+        height: '60px',
+        width: '150px',
+        border: '2px solid black ',
+        display: 'inline-block',
+        marginLeft: '10px',
+        backgroundColor: !on ? 'red' : 'white'
+    }
+    const indicatorStyle = {
+        height: '20px',
+        width: '20px',
+        border: '2px solid black ',
+        borderRadius: '50%',
+        display: 'inline-block',
+        marginLeft: '10px',
+        backgroundColor: on ? 'green' : 'red'
+    }
+
+    return (
+        <div>
+            <div style={onStyle} onClick={()=>{setOn(true)}}>On</div>
+            <div style={offStyle} onClick={()=>{setOn(false)}}>Off</div>
+            <div style={indicatorStyle}></div>
+        </div>
+    );
+};
