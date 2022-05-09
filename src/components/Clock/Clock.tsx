f5122e765d159157168aa45230b89c4d4a1a3b58
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-05-09 13:53:29 +0600

    lesson 23 clock finish

---
+++ src/components/Clock/Clock.tsx
@@ -0,0 +1,26 @@
+import React, {useEffect, useState} from 'react';
+
+
+type PropsType = {}
+
+export const Clock: React.FC<PropsType> = (props) => {
+
+const [date, setDate] = useState(new Date())
+
+    useEffect( ()=> {
+        const intervalID = setInterval(()=>{
+            setDate(new Date())
+        }, 1000)
+
+        return ()=> {
+            clearInterval(intervalID)
+        }
+    }, [])
+    return <div>
+        <span>{date.getHours()}</span>
+        :
+        <span>{date.getMinutes()}</span>
+        :
+        <span>{date.getSeconds()}</span>
+    </div>
+}
